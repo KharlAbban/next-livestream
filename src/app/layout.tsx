755fd3c2ebcf481be9302d7b44af59886cd45343
KharlAbban <khvngkharl123@gmail.com>
2025-05-02 14:02:10 +0000

    feat: Update Sanity schema types for improved type safety and query consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import { SanityLive } from "@/sanity/lib/live";
 
 export const metadata: Metadata = {
   title: "NextLive",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <html lang="en">
         <body className={`antialiased w-screen max-w-9xl overflow-x-hidden`}>
           {children}
+          <SanityLive />
           <Toaster richColors closeButton />
         </body>
       </html>
